e2344228bf13580622b5a0f78065a55e69862bfa
costariann <afuaoye30@gmail.com>
2025-02-21 13:02:25 +0000

    fix: update LoadingBox export and imports

--- frontend/src/component/LoadingBox.js
+++ frontend/src/component/LoadingBox.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Spinner from 'react-bootstrap/Spinner';
 
-export const LoadingBox = () => {
+// Change from named export to default export
+const LoadingBox = () => {
   return (
     <Spinner animation="border" role="status">
-      <span className="visually-hidden">Loading</span>
+      <span className="visually-hidden">Loading...</span>
     </Spinner>
   );
 };
+
+export default LoadingBox; // Change to default export
